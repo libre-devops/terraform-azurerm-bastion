978a77a32f218af9c929aa0c7f9dd1246d2b4c28
Craig Thacker <craigthackerx@gmail.com>
2022-05-01 00:52:19 +0100

    Update module

--- input.tf
+++ input.tf
@@ -1,22 +1,21 @@
-variable "vnet_name" {
+variable "bas_host_ipconfig_name" {
   type        = string
-  description = "The name of the VNet the bastion is intended to join"
+  description = "The IP Configuration name for the Azure Bastion"
 }
 
-variable "bas_subnet_name" {
-  default     = "AzureBastionSubnet"
+variable "bas_host_location" {
   type        = string
-  description = "The name of the Azure Bastion Subnet - note, this is a static value and should not be changed"
+  description = "The location for the bastion host, default is UK South"
 }
 
-variable "bas_subnet_iprange" {
+variable "bas_host_name" {
   type        = string
-  description = "The IP Range for the Bastion Subnet - Note, Minimum is a /28"
+  description = "The name for the Bastion host in the portal"
 }
 
-variable "bas_nsg_name" {
+variable "bas_host_rg_name" {
   type        = string
-  description = "The name for the NSG to be created with the AzureBastionSubnet"
+  description = "The resource group name for the Bastion resource"
 }
 
 variable "bas_nsg_location" {
@@ -24,23 +23,18 @@ variable "bas_nsg_location" {
   description = "The location of the bastion nsg"
 }
 
+variable "bas_nsg_name" {
+  type        = string
+  description = "The name for the NSG to be created with the AzureBastionSubnet"
+}
+
 variable "bas_nsg_rg_name" {
   description = "The resource group name which the NSG should be placed in"
 }
 
-variable "tags" {
-  description = "The default tags to be assigned"
-  type        = map(any)
-}
-
-variable "vnet_rg_name" {
-  type        = string
-  description = "The name of the resource group that the VNet can be found int"
-}
-
-variable "bas_pip_name" {
-  type        = string
-  description = "The name for the Bastion Public IP"
+variable "bas_pip_allocation_method" {
+  default     = "Static"
+  description = "The allocation method for the Public IP, default is Static"
 }
 
 variable "bas_pip_location" {
@@ -48,14 +42,14 @@ variable "bas_pip_location" {
   description = "The location for the Bastion Public IP, default is UK South"
 }
 
+variable "bas_pip_name" {
+  type        = string
+  description = "The name for the Bastion Public IP"
+}
+
 variable "bas_pip_rg_name" {
   type        = string
   description = "The resource group name for Bastion Public IP"
-}
-
-variable "bas_pip_allocation_method" {
-  default     = "Static"
-  description = "The allocation method for the Public IP, default is Static"
 }
 
 variable "bas_pip_sku" {
@@ -64,22 +58,28 @@ variable "bas_pip_sku" {
   description = "The SKU for the Bastion Public IP, default is Standard"
 }
 
-variable "bas_host_name" {
+variable "bas_subnet_iprange" {
   type        = string
-  description = "The name for the Bastion host in the portal"
+  description = "The IP Range for the Bastion Subnet - Note, Minimum is a /28"
 }
 
-variable "bas_host_location" {
+variable "bas_subnet_name" {
+  default     = "AzureBastionSubnet"
   type        = string
-  description = "The location for the bastion host, default is UK South"
+  description = "The name of the Azure Bastion Subnet - note, this is a static value and should not be changed"
 }
 
-variable "bas_host_rg_name" {
-  type        = string
-  description = "The resource group name for the Bastion resource"
+variable "tags" {
+  description = "The default tags to be assigned"
+  type        = map(any)
 }
 
-variable "bas_host_ipconfig_name" {
+variable "vnet_name" {
   type        = string
-  description = "The IP Configuration name for the Azure Bastion"
+  description = "The name of the VNet the bastion is intended to join"
+}
+
+variable "vnet_rg_name" {
+  type        = string
+  description = "The name of the resource group that the VNet can be found int"
 }
--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
+output "bas_hostname" {
+  value       = azurerm_bastion_host.bas_host.name
+  description = "The host name of the bastion"
+}
+
 output "bas_nsg_id" {
   value       = azurerm_network_security_group.bas_nsg.id
   description = "The host name of the bastion"
@@ -13,17 +18,12 @@ output "bas_subnet_id" {
   description = "The host name of the bastion"
 }
 
-output "bas_subnet_name" {
-  value       = azurerm_subnet.bas_subnet.name
-  description = "The subnet name of the Azure Bastion subnet"
-}
-
 output "bas_subnet_ip_range" {
   value       = azurerm_subnet.bas_subnet.address_prefixes
   description = "Bastion subnet IP range"
 }
 
-output "bas_hostname" {
-  value       = azurerm_bastion_host.bas_host.name
-  description = "The host name of the bastion"
+output "bas_subnet_name" {
+  value       = azurerm_subnet.bas_subnet.name
+  description = "The subnet name of the Azure Bastion subnet"
 }
